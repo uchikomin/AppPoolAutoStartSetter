ac227fb0cab125918dabda72e1ddbfedbfffe572
nakaochi.kohei <nakaochi.kohei@FCCLS0076552>
2025-07-10 17:59:18 +0900

    プロジェクト ファイルを追加します。

---
+++ AppPoolAutoStartSetter/FormMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AppPoolAutoStartSetter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDisableAll = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listViewAppPool = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderAutoStart = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonDisableAll);
+            this.panel1.Controls.Add(this.buttonLoad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 56);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonDisableAll
+            // 
+            this.buttonDisableAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDisableAll.Location = new System.Drawing.Point(275, 3);
+            this.buttonDisableAll.Name = "buttonDisableAll";
+            this.buttonDisableAll.Size = new System.Drawing.Size(109, 50);
+            this.buttonDisableAll.TabIndex = 0;
+            this.buttonDisableAll.Text = "すべて無効";
+            this.buttonDisableAll.UseVisualStyleBackColor = true;
+            this.buttonDisableAll.Click += new System.EventHandler(this.buttonDisableAll_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(3, 3);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(109, 50);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "読込";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listViewAppPool);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 56);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(387, 400);
+            this.panel2.TabIndex = 1;
+            // 
+            // listViewAppPool
+            // 
+            this.listViewAppPool.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderAutoStart});
+            this.listViewAppPool.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewAppPool.HideSelection = false;
+            this.listViewAppPool.Location = new System.Drawing.Point(0, 0);
+            this.listViewAppPool.Name = "listViewAppPool";
+            this.listViewAppPool.Size = new System.Drawing.Size(387, 400);
+            this.listViewAppPool.TabIndex = 0;
+            this.listViewAppPool.UseCompatibleStateImageBehavior = false;
+            this.listViewAppPool.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "アプリケーションプール名";
+            this.columnHeaderName.Width = 200;
+            // 
+            // columnHeaderAutoStart
+            // 
+            this.columnHeaderAutoStart.Text = "autoStart";
+            this.columnHeaderAutoStart.Width = 108;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 456);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(403, 495);
+            this.Name = "FormMain";
+            this.Text = "AppPool AutoStart Setter";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonDisableAll;
+        private System.Windows.Forms.ListView listViewAppPool;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderAutoStart;
+    }
+}
---
+++ AppPoolAutoStartSetter/FormMain.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppPoolAutoStartSetter
+{
+    public partial class FormMain : Form
+    {
+        /// <summary>
+        /// セクション名：アプリケーションプール
+        /// </summary>
+        private static readonly string APP_POOL_SECTION_NAME = "system.applicationHost/applicationPools";
+
+        /// <summary>
+        /// 要素名：アプリケーションプール名
+        /// </summary>
+        private static readonly string ELEM_NAME = "name";
+
+        /// <summary>
+        /// 要素名：autoStart
+        /// </summary>
+        private static readonly string ELEM_AUTO_START = "autoStart";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        // 参考
+        // https://learn.microsoft.com/ja-jp/iis/configuration/system.applicationhost/applicationpools/applicationpooldefaults/
+
+        private void LoadAppPool()
+        {
+            // 描画一時停止
+            listViewAppPool.BeginUpdate();
+
+            // ListViewの項目を削除
+            listViewAppPool.Items.Clear();
+
+            try
+            {
+                // ServerManager を取得
+                using (var manager = new ServerManager())
+                {
+                    // アプリケーションホスト設定を取得
+                    var config = manager.GetApplicationHostConfiguration();
+
+                    // アプリケーションプール情報を取得
+                    var appPoolSection = config.GetSection(APP_POOL_SECTION_NAME);
+
+                    // 各要素について繰り返し
+                    foreach (var elem in appPoolSection.ChildElements)
+                    {
+                        // アプリケーションプール名
+                        var name = elem[ELEM_NAME] as string;
+                        // autoStart の値
+                        var autoStart = elem[ELEM_AUTO_START] as bool?;
+
+                        // ListViewItem を生成
+                        var item = new ListViewItem();
+
+                        // テキストを設定
+                        item.Text = name;
+                        item.SubItems.Add(autoStart.HasValue ? autoStart.ToString() : true.ToString());
+
+                        // ListView に追加
+                        listViewAppPool.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("アプリケーションプールの読込に失敗しました: " + ex.Message);
+            }
+
+            // 描画再開
+            listViewAppPool.EndUpdate();
+
+            // すべて無効ボタンの有効化
+            buttonDisableAll.Enabled = listViewAppPool.Items.Count > 0;
+        }
+
+        /// <summary>
+        /// フォームを読み込んだときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            // すべて無効ボタンを無効化
+            buttonDisableAll.Enabled = false;
+        }
+
+        /// <summary>
+        /// 読込ボタンを押したときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            // アプリプールを読込
+            LoadAppPool();
+        }
+
+        /// <summary>
+        /// すべて無効を押したときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDisableAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ServerManager を取得
+                using (var manager = new ServerManager())
+                {
+                    // アプリケーションホスト設定を取得
+                    var config = manager.GetApplicationHostConfiguration();
+
+                    // アプリケーションプールのセクションを取得
+                    var appPoolSection = config.GetSection(APP_POOL_SECTION_NAME);
+
+                    // ListView の項目に対して繰り返し
+                    foreach (ListViewItem item in listViewAppPool.Items)
+                    {
+                        // アプリケーションプール名を取得
+                        var name = item.Text;
+                        // autoStart を取得
+                        var autoStart = bool.Parse(item.SubItems[0].Text);
+
+                        // アプリケーションプールの要素を取得
+                        var elem = appPoolSection.GetChildElement(name);
+
+                        // 要素が取得できなかった場合
+                        if (elem == null)
+                        {
+                            // スキップ
+                            continue;
+                        }
+
+                        // autoStart を false にする
+                        elem[ELEM_AUTO_START] = false;
+                    }
+
+                    // 変更を保存する
+                    manager.CommitChanges();
+                }
+
+                // アプリケーションプールを読込
+                LoadAppPool();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("アプリケーションプールの無効化に失敗しました: " + ex.Message);
+            }
+        }
+    }
+}
